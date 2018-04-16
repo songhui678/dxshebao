c2e5372e6849287cf82673e192be279287c699ca
songhui678 <songhui678@gmail.com>
2018-04-17 00:44:39 +0800

    111111

--- app/install/data/config.php
+++ app/install/data/config.php
@@ -7,19 +7,19 @@ return [
     // 数据库类型
     'type'     => 'mysql',
     // 服务器地址
-    'hostname' => '127.0.0.1',
+    'hostname' => '#hostname#',
     // 数据库名
-    'database' => 'dxshebao',
+    'database' => '#database#',
     // 用户名
-    'username' => 'root',
+    'username' => '#username#',
     // 密码
-    'password' => 'songhui@678',
+    'password' => '#password#',
     // 端口
-    'hostport' => '3306',
+    'hostport' => '#hostport#',
     // 数据库编码默认采用utf8
-    'charset'  => 'utf8mb4',
+    'charset'  => '#charset#',
     // 数据库表前缀
-    'prefix'   => 'cmf_',
-    "authcode" => 'yaQkG42N1WBTgtL8yP',
+    'prefix'   => '#prefix#',
+    "authcode" => '#authcode#',
     //#COOKIE_PREFIX#
 ];
