4c4e25fdf867bc6c01f01f5ed80d7518f2c5f6a3
songhui678 <songhui678@gmail.com>
2018-04-24 19:22:43 +0800

    no message

--- app/portal/controller/ListController.php
+++ app/portal/controller/ListController.php
@@ -10,23 +10,21 @@
 // +----------------------------------------------------------------------
 namespace app\portal\controller;
 
-use cmf\controller\HomeBaseController;
 use app\portal\model\PortalCategoryModel;
+use cmf\controller\HomeBaseController;
 
-class ListController extends HomeBaseController
-{
-    public function index()
-    {
-        $id                  = $this->request->param('id', 0, 'intval');
-        $portalCategoryModel = new PortalCategoryModel();
+class ListController extends HomeBaseController {
+	public function index() {
+		$id = $this->request->param('id', 0, 'intval');
+		$portalCategoryModel = new PortalCategoryModel();
 
-        $category = $portalCategoryModel->where('id', $id)->where('status', 1)->find();
-       
-        $this->assign('category', $category);
+		$category = $portalCategoryModel->where('id', $id)->where('status', 1)->find();
 
-        $listTpl = empty($category['list_tpl']) ? 'list' : $category['list_tpl'];
+		$this->assign('category', $category);
 
-        return $this->fetch('/' . $listTpl);
-    }
+		$listTpl = empty($category['list_tpl']) ? 'list' : $category['list_tpl'];
+
+		return $this->fetch('/' . $listTpl);
+	}
 
 }
