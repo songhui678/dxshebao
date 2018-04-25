749e4cad782f0813f6c3f86538d774976e3cdbb0
songhui678 <songhui678@gmail.com>
2018-04-26 01:55:44 +0800

    321

--- app/portal/controller/ShebaochangshiController.php
+++ app/portal/controller/ShebaochangshiController.php
@@ -9,12 +9,27 @@
 // | Author: 老猫 <thinkcmf@126.com>
 // +----------------------------------------------------------------------
 namespace app\portal\controller;
-
+use app\portal\model\PortalCategoryModel;
+use app\portal\service\PostService;
 use cmf\controller\HomeBaseController;
 
 class ShebaochangshiController extends HomeBaseController {
 	public function index() {
+		$id = $this->request->param('id', 0, 'intval');
+		$portalCategoryModel = new PortalCategoryModel();
+		if ($id == 0) {
+			$category = $portalCategoryModel->where('id', 1)->where('status', 1)->find();
+			$postService = new PostService();
+			$data = $postService->publishedAll();
+			$this->assign('articles', $data->items());
+			$this->assign('page', $data->render());
+			$this->assign('category', $category);
+			return $this->fetch(':shebaochangshi_index');
+		} else {
+			$category = $portalCategoryModel->where('id', $id)->where('status', 1)->find();
+			$this->assign('category', $category);
+			return $this->fetch(':shebaochangshi');
+		}
 
-		return $this->fetch(':shebaochangshi');
 	}
 }
--- app/portal/service/PostService.php
+++ app/portal/service/PostService.php
@@ -12,221 +12,225 @@ namespace app\portal\service;
 
 use app\portal\model\PortalPostModel;
 
-class PostService
-{
+class PostService {
 
-    public function adminArticleList($filter)
-    {
-        return $this->adminPostList($filter);
-    }
+	public function adminArticleList($filter) {
+		return $this->adminPostList($filter);
+	}
 
-    public function adminPageList($filter)
-    {
-        return $this->adminPostList($filter, true);
-    }
+	public function adminPageList($filter) {
+		return $this->adminPostList($filter, true);
+	}
 
-    public function adminPostList($filter, $isPage = false)
-    {
+	public function adminPostList($filter, $isPage = false) {
 
-        $where = [
-            'a.create_time' => ['>=', 0],
-            'a.delete_time' => 0
-        ];
+		$where = [
+			'a.create_time' => ['>=', 0],
+			'a.delete_time' => 0,
+		];
 
-        $join = [
-            ['__USER__ u', 'a.user_id = u.id']
-        ];
+		$join = [
+			['__USER__ u', 'a.user_id = u.id'],
+		];
 
-        $field = 'a.*,u.user_login,u.user_nickname,u.user_email';
+		$field = 'a.*,u.user_login,u.user_nickname,u.user_email';
 
-        $category = empty($filter['category']) ? 0 : intval($filter['category']);
-        if (!empty($category)) {
-            $where['b.category_id'] = ['eq', $category];
-            array_push($join, [
-                '__PORTAL_CATEGORY_POST__ b', 'a.id = b.post_id'
-            ]);
-            $field = 'a.*,b.id AS post_category_id,b.list_order,b.category_id,u.user_login,u.user_nickname,u.user_email';
-        }
+		$category = empty($filter['category']) ? 0 : intval($filter['category']);
+		if (!empty($category)) {
+			$where['b.category_id'] = ['eq', $category];
+			array_push($join, [
+				'__PORTAL_CATEGORY_POST__ b', 'a.id = b.post_id',
+			]);
+			$field = 'a.*,b.id AS post_category_id,b.list_order,b.category_id,u.user_login,u.user_nickname,u.user_email';
+		}
 
-        $startTime = empty($filter['start_time']) ? 0 : strtotime($filter['start_time']);
-        $endTime   = empty($filter['end_time']) ? 0 : strtotime($filter['end_time']);
-        if (!empty($startTime) && !empty($endTime)) {
-            $where['a.published_time'] = [['>= time', $startTime], ['<= time', $endTime]];
-        } else {
-            if (!empty($startTime)) {
-                $where['a.published_time'] = ['>= time', $startTime];
-            }
-            if (!empty($endTime)) {
-                $where['a.published_time'] = ['<= time', $endTime];
-            }
-        }
+		$startTime = empty($filter['start_time']) ? 0 : strtotime($filter['start_time']);
+		$endTime = empty($filter['end_time']) ? 0 : strtotime($filter['end_time']);
+		if (!empty($startTime) && !empty($endTime)) {
+			$where['a.published_time'] = [['>= time', $startTime], ['<= time', $endTime]];
+		} else {
+			if (!empty($startTime)) {
+				$where['a.published_time'] = ['>= time', $startTime];
+			}
+			if (!empty($endTime)) {
+				$where['a.published_time'] = ['<= time', $endTime];
+			}
+		}
 
-        $keyword = empty($filter['keyword']) ? '' : $filter['keyword'];
-        if (!empty($keyword)) {
-            $where['a.post_title'] = ['like', "%$keyword%"];
-        }
+		$keyword = empty($filter['keyword']) ? '' : $filter['keyword'];
+		if (!empty($keyword)) {
+			$where['a.post_title'] = ['like', "%$keyword%"];
+		}
 
-        if ($isPage) {
-            $where['a.post_type'] = 2;
-        } else {
-            $where['a.post_type'] = 1;
-        }
+		if ($isPage) {
+			$where['a.post_type'] = 2;
+		} else {
+			$where['a.post_type'] = 1;
+		}
 
-        $portalPostModel = new PortalPostModel();
-        $articles        = $portalPostModel->alias('a')->field($field)
-            ->join($join)
-            ->where($where)
-            ->order('update_time', 'DESC')
-            ->paginate(10);
+		$portalPostModel = new PortalPostModel();
+		$articles = $portalPostModel->alias('a')->field($field)
+			->join($join)
+			->where($where)
+			->order('update_time', 'DESC')
+			->paginate(10);
 
-        return $articles;
+		return $articles;
 
-    }
+	}
 
-    public function publishedArticle($postId, $categoryId = 0)
-    {
-        $portalPostModel = new PortalPostModel();
+	public function publishedArticle($postId, $categoryId = 0) {
+		$portalPostModel = new PortalPostModel();
 
-        if (empty($categoryId)) {
+		if (empty($categoryId)) {
 
-            $where = [
-                'post.post_type'      => 1,
-                'post.published_time' => [['< time', time()], ['> time', 0]],
-                'post.post_status'    => 1,
-                'post.delete_time'    => 0,
-                'post.id'             => $postId
-            ];
+			$where = [
+				'post.post_type' => 1,
+				'post.published_time' => [['< time', time()], ['> time', 0]],
+				'post.post_status' => 1,
+				'post.delete_time' => 0,
+				'post.id' => $postId,
+			];
 
-            $article = $portalPostModel->alias('post')->field('post.*')
-                ->where($where)
-                ->find();
-        } else {
-            $where = [
-                'post.post_type'       => 1,
-                'post.published_time'  => [['< time', time()], ['> time', 0]],
-                'post.post_status'     => 1,
-                'post.delete_time'     => 0,
-                'relation.category_id' => $categoryId,
-                'relation.post_id'     => $postId
-            ];
+			$article = $portalPostModel->alias('post')->field('post.*')
+				->where($where)
+				->find();
+		} else {
+			$where = [
+				'post.post_type' => 1,
+				'post.published_time' => [['< time', time()], ['> time', 0]],
+				'post.post_status' => 1,
+				'post.delete_time' => 0,
+				'relation.category_id' => $categoryId,
+				'relation.post_id' => $postId,
+			];
 
-            $join    = [
-                ['__PORTAL_CATEGORY_POST__ relation', 'post.id = relation.post_id']
-            ];
-            $article = $portalPostModel->alias('post')->field('post.*')
-                ->join($join)
-                ->where($where)
-                ->find();
-        }
+			$join = [
+				['__PORTAL_CATEGORY_POST__ relation', 'post.id = relation.post_id'],
+			];
+			$article = $portalPostModel->alias('post')->field('post.*')
+				->join($join)
+				->where($where)
+				->find();
+		}
 
+		return $article;
+	}
 
-        return $article;
-    }
+	//上一篇文章
+	public function publishedPrevArticle($postId, $categoryId = 0) {
+		$portalPostModel = new PortalPostModel();
 
-    //上一篇文章
-    public function publishedPrevArticle($postId, $categoryId = 0)
-    {
-        $portalPostModel = new PortalPostModel();
+		if (empty($categoryId)) {
 
-        if (empty($categoryId)) {
+			$where = [
+				'post.post_type' => 1,
+				'post.published_time' => [['< time', time()], ['> time', 0]],
+				'post.post_status' => 1,
+				'post.delete_time' => 0,
+				'post.id ' => ['<', $postId],
+			];
 
-            $where = [
-                'post.post_type'      => 1,
-                'post.published_time' => [['< time', time()], ['> time', 0]],
-                'post.post_status'    => 1,
-                'post.delete_time'    => 0,
-                'post.id '            => ['<', $postId]
-            ];
+			$article = $portalPostModel->alias('post')->field('post.*')
+				->where($where)
+				->order('id', 'DESC')
+				->find();
 
-            $article = $portalPostModel->alias('post')->field('post.*')
-                ->where($where)
-                ->order('id', 'DESC')
-                ->find();
+		} else {
+			$where = [
+				'post.post_type' => 1,
+				'post.published_time' => [['< time', time()], ['> time', 0]],
+				'post.post_status' => 1,
+				'post.delete_time' => 0,
+				'relation.category_id' => $categoryId,
+				'relation.post_id' => ['<', $postId],
+			];
 
-        } else {
-            $where = [
-                'post.post_type'       => 1,
-                'post.published_time'  => [['< time', time()], ['> time', 0]],
-                'post.post_status'     => 1,
-                'post.delete_time'     => 0,
-                'relation.category_id' => $categoryId,
-                'relation.post_id'     => ['<', $postId]
-            ];
+			$join = [
+				['__PORTAL_CATEGORY_POST__ relation', 'post.id = relation.post_id'],
+			];
+			$article = $portalPostModel->alias('post')->field('post.*')
+				->join($join)
+				->where($where)
+				->order('id', 'DESC')
+				->find();
+		}
 
-            $join    = [
-                ['__PORTAL_CATEGORY_POST__ relation', 'post.id = relation.post_id']
-            ];
-            $article = $portalPostModel->alias('post')->field('post.*')
-                ->join($join)
-                ->where($where)
-                ->order('id', 'DESC')
-                ->find();
-        }
+		return $article;
+	}
 
+	//下一篇文章
+	public function publishedNextArticle($postId, $categoryId = 0) {
+		$portalPostModel = new PortalPostModel();
 
-        return $article;
-    }
+		if (empty($categoryId)) {
 
-    //下一篇文章
-    public function publishedNextArticle($postId, $categoryId = 0)
-    {
-        $portalPostModel = new PortalPostModel();
+			$where = [
+				'post.post_type' => 1,
+				'post.published_time' => [['< time', time()], ['> time', 0]],
+				'post.post_status' => 1,
+				'post.delete_time' => 0,
+				'post.id' => ['>', $postId],
+			];
 
-        if (empty($categoryId)) {
+			$article = $portalPostModel->alias('post')->field('post.*')
+				->where($where)
+				->order('id', 'ASC')
+				->find();
+		} else {
+			$where = [
+				'post.post_type' => 1,
+				'post.published_time' => [['< time', time()], ['> time', 0]],
+				'post.post_status' => 1,
+				'post.delete_time' => 0,
+				'relation.category_id' => $categoryId,
+				'relation.post_id' => ['>', $postId],
+			];
 
-            $where = [
-                'post.post_type'      => 1,
-                'post.published_time' => [['< time', time()], ['> time', 0]],
-                'post.post_status'    => 1,
-                'post.delete_time'    => 0,
-                'post.id'             => ['>', $postId]
-            ];
+			$join = [
+				['__PORTAL_CATEGORY_POST__ relation', 'post.id = relation.post_id'],
+			];
+			$article = $portalPostModel->alias('post')->field('post.*')
+				->join($join)
+				->where($where)
+				->order('id', 'ASC')
+				->find();
+		}
 
-            $article = $portalPostModel->alias('post')->field('post.*')
-                ->where($where)
-                ->order('id', 'ASC')
-                ->find();
-        } else {
-            $where = [
-                'post.post_type'       => 1,
-                'post.published_time'  => [['< time', time()], ['> time', 0]],
-                'post.post_status'     => 1,
-                'post.delete_time'     => 0,
-                'relation.category_id' => $categoryId,
-                'relation.post_id'     => ['>', $postId]
-            ];
+		return $article;
+	}
 
-            $join    = [
-                ['__PORTAL_CATEGORY_POST__ relation', 'post.id = relation.post_id']
-            ];
-            $article = $portalPostModel->alias('post')->field('post.*')
-                ->join($join)
-                ->where($where)
-                ->order('id', 'ASC')
-                ->find();
-        }
+	public function publishedPage($pageId) {
 
+		$where = [
+			'post_type' => 2,
+			'published_time' => [['< time', time()], ['> time', 0]],
+			'post_status' => 1,
+			'delete_time' => 0,
+			'id' => $pageId,
+		];
 
-        return $article;
-    }
+		$portalPostModel = new PortalPostModel();
+		$page = $portalPostModel
+			->where($where)
+			->find();
 
-    public function publishedPage($pageId)
-    {
+		return $page;
+	}
+	public function publishedAll() {
 
-        $where = [
-            'post_type'      => 2,
-            'published_time' => [['< time', time()], ['> time', 0]],
-            'post_status'    => 1,
-            'delete_time'    => 0,
-            'id'             => $pageId
-        ];
+		$where = [
+			'post_type' => 1,
+			'published_time' => [['< time', time()], ['> time', 0]],
+			'post_status' => 1,
+			'delete_time' => 0,
+		];
+		$portalPostModel = new PortalPostModel();
+		$articles = $portalPostModel->alias('a')->field('*')
+			->where($where)
+			->order('update_time', 'DESC')
+			->paginate(10);
 
-        $portalPostModel = new PortalPostModel();
-        $page            = $portalPostModel
-            ->where($where)
-            ->find();
-
-        return $page;
-    }
-
+		return $articles;
+	}
 }
